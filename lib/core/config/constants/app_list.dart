e1c14d5533c6cade1c0286bdcb3c33735ee858b3
Hamza Qasim <hamzaqasim.co@gmail.com>
2025-03-15 18:01:14 +0500

    added new skills

--- lib/core/config/constants/app_list.dart
+++ lib/core/config/constants/app_list.dart
@@ -23,7 +23,8 @@ class AppLists {
     },
     {
       'title': 'Water Consumption Tracker',
-      'description': 'A mobile app designed to track and manage water consumption, promoting sustainability and eco-friendliness.',
+      'description':
+          'A mobile app designed to track and manage water consumption, promoting sustainability and eco-friendliness.',
       'image': 'assets/images/water_consumption_tracker.png',
       'url': 'https://github.com/HamzaQasim7/water_consumption_app'
     },
@@ -36,13 +37,76 @@ class AppLists {
     },
   ];
 
-  static final skills = [
+  static final topSkills = [
     {'name': 'Flutter', 'img': 'assets/icons/flutter-logo.svg'},
     {'name': 'Dart', 'img': 'assets/icons/dartlang-icon.svg'},
     {'name': 'Firebase', 'img': 'assets/icons/firebase.svg'},
     {'name': 'REST APIs', 'img': 'assets/icons/api.svg'},
-    {'name': 'UI/UX Design', 'img': 'assets/icons/uiux.svg'},
     {'name': 'Provider', 'img': 'assets/icons/provider.svg'},
+    {'name': 'UI/UX Design', 'img': 'assets/icons/uiux.svg'},
+  ];
+
+  static final allSkills = [
+    // State Management
+    {'name': 'Bloc', 'img': 'assets/icons/bloc.svg'},
+    {'name': 'GetX', 'img': 'assets/icons/getx.svg'},
+    {'name': 'Provider', 'img': 'assets/icons/riverpod.svg'},
+
+    // Databases & Backend
+    {'name': 'SQLite', 'img': 'assets/icons/sqlite.svg'},
+    {'name': 'Hive', 'img': 'assets/icons/mongodb.svg'},
+    {'name': 'Node.js', 'img': 'assets/icons/nodejs.svg'},
+    {'name': 'Firebase', 'img': 'assets/assets/icons/firebase.svg'},
+    {'name': 'Supabase', 'img': 'assets/icons/supabase.svg'},
+
+    // Development Tools
+    {'name': 'Git', 'img': 'assets/icons/git.svg'},
+    {'name': 'GitHub', 'img': 'assets/icons/github.svg'},
+    {'name': 'VS Code', 'img': 'assets/icons/vscode.svg'},
+    {'name': 'Android Studio', 'img': 'assets/icons/android-studio.svg'},
+
+    // Testing & Quality
+    {'name': 'Unit Testing', 'img': 'assets/icons/testing.svg'},
+    {'name': 'Widget Testing', 'img': 'assets/icons/widget-testing.svg'},
+    {
+      'name': 'Integration Testing',
+      'img': 'assets/icons/integration-testing.svg'
+    },
+
+    // Design & UI/UX
+    {'name': 'Figma', 'img': 'assets/icons/figma.svg'},
+    {'name': 'Adobe XD', 'img': 'assets/icons/adobe-xd.svg'},
+    {'name': 'Material Design', 'img': 'assets/icons/material-design.svg'},
+
+    // Real-time Technologies
     {'name': 'WebRTC', 'img': 'assets/icons/webrtc.svg'},
+    {'name': 'WebSocket', 'img': 'assets/icons/websocket.svg'},
+    {'name': 'Stream', 'img': 'assets/icons/stream.svg'},
+
+    // Cloud Services
+    {'name': 'Google Cloud', 'img': 'assets/icons/google-cloud.svg'},
+
+    // CI/CD
+    {'name': 'GitHub Actions', 'img': 'assets/icons/github-actions.svg'},
+    {'name': 'Codemagic', 'img': 'assets/icons/codemagic.svg'},
+
+    // App Performance
+    {'name': 'App Optimization', 'img': 'assets/icons/optimization.svg'},
+    {'name': 'Performance Profiling', 'img': 'assets/icons/profiling.svg'},
+
+    // Cross-Platform
+    {'name': 'iOS Development', 'img': 'assets/icons/ios.svg'},
+    {'name': 'Android Development', 'img': 'assets/icons/android.svg'},
+    {'name': 'Web Development', 'img': 'assets/icons/web.svg'},
+
+    // Additional Programming Languages
+    {'name': 'C++', 'img': 'assets/icons/javascript.svg'},
+    {'name': 'Python', 'img': 'assets/icons/python.svg'},
+    {'name': 'Java', 'img': 'assets/icons/java.svg'},
+
+    // Project Management
+    {'name': 'Agile', 'img': 'assets/icons/agile.svg'},
+    {'name': 'Scrum', 'img': 'assets/icons/scrum.svg'},
+    {'name': 'Slack', 'img': 'assets/icons/scrum.svg'}
   ];
 }
--- lib/screens/skills/skill_section.dart
+++ lib/screens/skills/skill_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 import 'package:hamza_folio/core/config/constants/app_list.dart';
 import 'package:hamza_folio/widgets/app_header_text.dart';
 import 'package:responsive_framework/responsive_framework.dart';
@@ -10,7 +11,6 @@ class SkillsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textContext = Theme.of(context).textTheme;
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 45),
       child: Column(
@@ -29,16 +29,270 @@ class SkillsSection extends StatelessWidget {
             ),
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemCount: AppLists.skills.length,
+            itemCount: AppLists.topSkills.length,
             itemBuilder: (context, index) {
               return SkillCardWidget(
-                img: AppLists.skills[index]['img'] as String,
-                name: AppLists.skills[index]['name'] as String,
+                img: AppLists.topSkills[index]['img'] as String,
+                name: AppLists.topSkills[index]['name'] as String,
               );
             },
           ),
+          const SizedBox(height: 32),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => const AllSkillsDialog(),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 16,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                'View All Skills',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+// Create a new dialog to show all skills
+class AllSkillsDialog extends StatelessWidget {
+  const AllSkillsDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: GlassmorphicContainer(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.8,
+        borderRadius: 20,
+        blur: 20,
+        border: 2,
+        linearGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Theme.of(context).colorScheme.surface.withOpacity(0.9),
+            Theme.of(context).colorScheme.surface.withOpacity(0.8),
+          ],
+        ),
+        borderGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Theme.of(context).colorScheme.surface.withOpacity(0.5),
+            Theme.of(context).colorScheme.surface.withOpacity(0.5),
+          ],
+        ),
+        child: Column(
+          children: [
+            // Dialog Header
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'All Skills',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(),
+            // Skills Grid
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _SkillCategory(
+                      title: 'Core Technologies',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'Flutter',
+                                'Dart',
+                                'Firebase',
+                                'REST APIs'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'State Management',
+                      skills: AppLists.allSkills
+                          .where((skill) => ['Provider', 'Bloc', 'GetX']
+                              .contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Databases & Backend',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'SQLite',
+                                'Hive',
+                                'Firebase',
+                                'Supabase'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Development Tools',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'Git',
+                                'GitHub',
+                                'VS Code',
+                                'Android Studio'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Testing & Quality',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'Unit Testing',
+                                'Widget Testing',
+                                'Integration Testing'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Design & UI/UX',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'UI/UX Design',
+                                'Figma',
+                                'Adobe XD',
+                                'Material Design'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Real-time Technologies',
+                      skills: AppLists.allSkills
+                          .where((skill) => ['WebRTC', 'WebSocket', 'Stream']
+                              .contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Cloud Services',
+                      skills: AppLists.allSkills
+                          .where((skill) =>
+                              ['Google Cloud'].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'CI/CD',
+                      skills: AppLists.allSkills
+                          .where((skill) => ['GitHub Actions', 'Codemagic']
+                              .contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'App Performance',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'App Optimization',
+                                'Performance Profiling'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Cross-Platform',
+                      skills: AppLists.allSkills
+                          .where((skill) => [
+                                'iOS Development',
+                                'Android Development',
+                                'Web Development'
+                              ].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Additional Programming Languages',
+                      skills: AppLists.allSkills
+                          .where((skill) =>
+                              ['C++', 'Python', 'Java'].contains(skill['name']))
+                          .toList(),
+                    ),
+                    _SkillCategory(
+                      title: 'Project Management',
+                      skills: AppLists.allSkills
+                          .where((skill) => ['Agile', 'Scrum', 'Slack']
+                              .contains(skill['name']))
+                          .toList(),
+                    ),
+                    const SizedBox(height: 32), // Bottom padding
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Helper widget for skill categories
+class _SkillCategory extends StatelessWidget {
+  final String title;
+  final List<Map<String, String>> skills;
+
+  const _SkillCategory({
+    required this.title,
+    required this.skills,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        ResponsiveGridView.builder(
+          gridDelegate: const ResponsiveGridDelegate(
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            minCrossAxisExtent: 100,
+            maxCrossAxisExtent: 150,
+            crossAxisExtent: 120,
+          ),
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: skills.length,
+          itemBuilder: (context, index) {
+            return SkillCardWidget(
+              img: skills[index]['img']!,
+              name: skills[index]['name']!,
+            );
+          },
+        ),
+      ],
     );
   }
 }
